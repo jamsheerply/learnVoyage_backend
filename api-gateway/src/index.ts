0ca789af34bc27ca189ccba0789aa10f5787886c
jamsheerply <jamsheerpayyoli@gmail.com>
2024-05-16 11:07:35 +0530

    feat:api_gateway setup

---
+++ api-gateway/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port: number = Number(process.env.PORT) || 3000;
+
+const app: Application = express();
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+});

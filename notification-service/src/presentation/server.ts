25cf4a69c4c6e33ad953b9f12174924615c9f40d
jamsheerply <jamsheerpayyoli@gmail.com>
2024-08-31 04:27:16 +0530

    fix:deployement error

--- notification-service/src/presentation/server.ts
+++ notification-service/src/presentation/server.ts
@@ -11,11 +11,25 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-
-app.use("/api", notificationRoute);
+app.use("/", (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    status: 404,
+    message: " notification is on ",
+  });
+});
+app.use("/api/notification", notificationRoute);
 
 const PORT = process.env.PORT || 3003;
-
+app.use("*", (req: Request, res: Response) => {
+  res
+    .status(404)
+    .json({
+      success: false,
+      status: 404,
+      message: "Api Not found notification",
+    });
+});
 app.listen(PORT, async () => {
   console.log(`notification-server is running on port ${PORT}`);
   startConsumer("notification-service");
